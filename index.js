e3699a68ae30ffa425774a3219451eca3c2070c6
David <bischof.david.db@gmail.com>
2024-04-21 22:15:00 +0200

    added logic so the users can join and recive a emit into the room of who is in this room currently

--- index.js
+++ index.js
@@ -17,36 +17,59 @@ const io = new Server(server, {
 
 const activeRooms = new Set();
 
+function getUsersInRoom(roomName) {
+    const clients = io.sockets.adapter.rooms.get(roomName);
+    if (!clients) {
+        return [];
+    }
+    return Array.from(clients).map(socketId => io.sockets.sockets.get(socketId).id);
+}
+
 io.on('connection', (socket) => {
     console.log('a user connected: ' + socket.id);
-    socket.on('disconnect', (room) => {
+    socket.on('disconnect', () => {
         console.log('user disconnected: ' + socket.id);
 
     });
     socket.on('create', (room) => {
         console.log('create room: ' + room);
-        socket.join(room);
-        activeRooms.add(room);
+        let newRoom = generateRandomRoom();
+        while (activeRooms.has(newRoom)) {
+            newRoom = generateRandomRoom();
+        }
+        socket.join(newRoom);
+        activeRooms.add(newRoom);
         console.log(activeRooms);
+        io.to(socket.id).emit('created', newRoom); // Emit to specific user id
     });
+
+    function generateRandomRoom() {
+        const digits = 6;
+        let room = '';
+        for (let i = 0; i < digits; i++) {
+            room += Math.floor(Math.random() * 10);
+        }
+        return room;
+    }
 
     socket.on('join', (room) => {
         console.log('join room: ' + room);
         if (activeRooms.has(room)) {
             socket.join(room);
-            io.emit('joined', room);
+            const usersInRoom = getUsersInRoom(room);
+            io.to(room).emit('joined', { room, users: usersInRoom });
         } else {
-            console.log('Room does not exist: ' + room);
-            io.emit('room does not exist', room);
+            io.to(socket.id).emit('room does not exist', room);
         }
     });
+
+
     socket.on('check if room exists', (room) => {
         console.log('check if room exists: ' + room);
         if (activeRooms.has(room)) {
-            io.emit('room exists', room);
-            console.log('Was checked by ' + socket.id);
+            io.to(socket.id).emit('room exists', room);
         } else {
-            io.emit('room does not exist', room);
+            io.to(socket.id).emit('room does not exist', room);
         }
     });
 }
