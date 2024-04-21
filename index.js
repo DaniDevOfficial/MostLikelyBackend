3ac42eaab51070baa812ebcd86532f477f873852
David <bischof.david.db@gmail.com>
2024-04-21 20:35:34 +0200

    added some check logic

--- index.js
+++ index.js
@@ -15,25 +15,44 @@ const io = new Server(server, {
     }
 });
 
+const activeRooms = new Set();
 
 io.on('connection', (socket) => {
     console.log('a user connected: ' + socket.id);
-    socket.on('disconnect', () => {
+    socket.on('disconnect', (room) => {
         console.log('user disconnected: ' + socket.id);
+
     });
+    socket.on('create', (room) => {
+        console.log('create room: ' + room);
+        socket.join(room);
+        activeRooms.add(room);
+        console.log(activeRooms);
+    });
+
     socket.on('join', (room) => {
         console.log('join room: ' + room);
-        socket.join(room);
+        if (activeRooms.has(room)) {
+            socket.join(room);
+            io.emit('joined', room);
+        } else {
+            console.log('Room does not exist: ' + room);
+            io.emit('room does not exist', room);
+        }
     });
-    socket.on('message', (msg) => {
-        console.log('message: ' + msg);
-        socket.broadcast.emit('broadcast message', msg);
-    }
-    );
+    socket.on('check if room exists', (room) => {
+        console.log('check if room exists: ' + room);
+        if (activeRooms.has(room)) {
+            io.emit('room exists', room);
+            console.log('Was checked by ' + socket.id);
+        } else {
+            io.emit('room does not exist', room);
+        }
+    });
 }
 );
 
 
 server.listen(port, () => {
     console.log(`listening on Port ${port}`);
-});
+});  
