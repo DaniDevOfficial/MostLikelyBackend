5d8c9911a8c9523f3be8d5270b4e5597734a4f67
David <bischof.david.db@gmail.com>
2024-04-22 13:38:34 +0200

    added wayy better user handling so if a user disconnects it gets updated on the other instances and if a room is empty it gets deleted

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const io = new Server(server, {
 });
 
 const activeRooms = new Set();
+const userRooms = {};
 
 function getUsersInRoom(roomName) {
     const clients = io.sockets.adapter.rooms.get(roomName);
@@ -25,39 +26,97 @@ function getUsersInRoom(roomName) {
     return Array.from(clients).map(socketId => io.sockets.sockets.get(socketId).id);
 }
 
+function generateRandomRoom() {
+    const digits = 6;
+    let room = '';
+    for (let i = 0; i < digits; i++) {
+        room += Math.floor(Math.random() * 10);
+    }
+    return room;
+}
+
+function addUserRoom(userId, roomId) {
+    if (!userRooms[userId]) {
+        userRooms[userId] = [roomId];
+    } else {
+        if (!userRooms[userId].includes(roomId)) {
+            userRooms[userId].push(roomId);
+        }
+    }
+}
+
+function removeUserRoom(userId, roomId) {
+    if (userRooms[userId]) {
+        const index = userRooms[userId].indexOf(roomId);
+        if (index !== -1) {
+            userRooms[userId].splice(index, 1);
+            if (userRooms[userId].length === 0) {
+                delete userRooms[userId];
+            }
+        }
+    }
+}
+function checkIfRoomIsEmpty(room) {
+    const clients = io.sockets.adapter.rooms.get(room);
+    if (!clients) {
+        return true;
+    }
+    return Array.from(clients).length === 0;
+}
+
+function getUserRooms(userId) {
+    return userRooms[userId] || [];
+}
 io.on('connection', (socket) => {
+
+
     console.log('a user connected: ' + socket.id);
     socket.on('disconnect', () => {
         console.log('user disconnected: ' + socket.id);
-
+        const rooms = getUserRooms(socket.id);
+        rooms.forEach(room => {
+            socket.leave(room);
+            const usersInRoom = getUsersInRoom(room);
+            if (checkIfRoomIsEmpty(room)) {
+                activeRooms.delete(room);
+                console.log('room is empty: ' + room);
+            }
+            io.to(room).emit('left', { room, users: usersInRoom });
+        });
+        delete userRooms[socket.id];
     });
     socket.on('create', (room) => {
-        console.log('create room: ' + room);
         let newRoom = generateRandomRoom();
         while (activeRooms.has(newRoom)) {
             newRoom = generateRandomRoom();
         }
         socket.join(newRoom);
         activeRooms.add(newRoom);
-        console.log(activeRooms);
-        io.to(socket.id).emit('created', newRoom); // Emit to specific user id
+        console.log('create room' + newRoom);
+        io.to(socket.id).emit('created', newRoom);
     });
 
-    function generateRandomRoom() {
-        const digits = 6;
-        let room = '';
-        for (let i = 0; i < digits; i++) {
-            room += Math.floor(Math.random() * 10);
+    socket.on('leave', (room) => {
+        socket.leave(room);
+        removeUserRoom(socket.id, room);
+        const usersInRoom = getUsersInRoom(room);
+        if (checkIfRoomIsEmpty(room)) {
+            activeRooms.delete(room);
+            console.log('deleted room: ' + room);
         }
-        return room;
-    }
+        io.to(room).emit('left', { room, users: usersInRoom });
+    });
 
     socket.on('join', (room) => {
-        console.log('join room: ' + room);
         if (activeRooms.has(room)) {
             socket.join(room);
+            console.log(socket.rooms);
             const usersInRoom = getUsersInRoom(room);
+            console.log("users in room: " + room + " " + usersInRoom);
+
             io.to(room).emit('joined', { room, users: usersInRoom });
+            addUserRoom(socket.id, room);
+
         } else {
             io.to(socket.id).emit('room does not exist', room);
         }
@@ -65,7 +124,6 @@ io.on('connection', (socket) => {
 
 
     socket.on('check if room exists', (room) => {
-        console.log('check if room exists: ' + room);
         if (activeRooms.has(room)) {
             io.to(socket.id).emit('room exists', room);
         } else {
